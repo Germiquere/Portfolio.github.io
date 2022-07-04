d797dd710b9efed71cc7b7e59704f56c9695131c
Germiquere <german150703.miquere@gmail.com>
2022-07-04 20:36:04 -0300

    agregando about me y contact me

--- app.js
+++ app.js
@@ -5,22 +5,44 @@
     
 // }
 
-const toggleTheme = document.getElementById("toggle-theme");
+const toggleTheme = document.getElementById("toggle");
 const toggleIcon = document.getElementById("toggle-img");
 const toggleText = document.getElementById("toggle-text");
 const prueba = document.getElementById('luz');
+const lampara = document.getElementById('lampara');
+const indicator = document.getElementById("indicator");
 
-toggleTheme.addEventListener('click', ()=>{
-    document.body.classList.toggle('dark');
-    if(toggleIcon.src.includes('night.png')){
-        toggleIcon.src="assets/sun.png";
-        toggleText.textContent= "Light Mode";
+//  function themeChange(){
+//     document.body.classList.toggle('dark');
+//     if(toggleIcon.src.includes('night.png')){
+
+//         toggleIcon.src="assets/sun.png";
+//         toggleText.textContent= "Light Mode";
+//         prueba.style.opacity = '0';
+//         prueba.style.animation = "none";
+//     }
+//     else{
+        
+//         toggleIcon.src= "assets/night.png";
+//         toggleText.textContent= "Dark Mode";
+//         prueba.style.animation = "intermitente 3s";
+//         prueba.style.opacity = '1';
+//     }
+// }
+function themeChange(){
+   if (document.body.classList.toggle('dark')){
+    prueba.style.animation = "intermitente 3s";
+    prueba.style.opacity = '1';
+    indicator.style.left = "0px";
+   }
+   else{
+    
         prueba.style.opacity = '0';
-    }
-    else{
-        toggleIcon.src= "assets/night.png";
-        toggleText.textContent= "Dark Mode";
-        prueba.style.animation = "intermitente2 3s";
-        prueba.style.opacity = '1';
-    }
-})
+        prueba.style.animation = "none";
+        indicator.style.left = "30px";
+
+}
+
+}
+toggleTheme.addEventListener('click', themeChange );
+lampara.addEventListener('click', themeChange);
